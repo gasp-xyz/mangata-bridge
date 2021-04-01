0f6fa30cbd5c353390de52a9a9b6ac24b1b5dfaa
iStrike7 <siddiquishoeb.iitm@gmail.com>
2021-04-01 18:45:08 +0530

    typo

--- ethereum/scripts/dumpAddresses.js
+++ ethereum/scripts/dumpAddresses.js
@@ -10,13 +10,13 @@ module.exports = async () => {
         const eth20AppInstance = await ETHApp.deployed()
         const erc20AppInstance = await ERC20App.deployed()
         const tokenInstance = await TestToken.deployed();
-				const mangataTokenInstance = await MangataToken.deployed();
+        const mangataTokenInstance = await MangataToken.deployed();
 
         const address = {
             ETHApp: eth20AppInstance.address,
             ERC20App: erc20AppInstance.address,
             TestToken: tokenInstance.address,
-						MangataToken: mangataTokenInstance.address
+            MangataToken: mangataTokenInstance.address
         }
 
         fs.writeFileSync("../test/build/address.json", JSON.stringify(address, null, 2))
