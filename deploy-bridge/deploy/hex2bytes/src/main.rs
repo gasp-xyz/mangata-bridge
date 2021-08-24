f8b0cb871345bb9879eb0e72206050d7d5797e0f
Ondrej Maksi <ondrej.maksi@gmail.com>
2021-08-24 10:18:41 +0200

    fixed hex2bytes

--- deploy-bridge/deploy/hex2bytes/src/main.rs
+++ deploy-bridge/deploy/hex2bytes/src/main.rs
@@ -1,11 +1,9 @@
 use std::env::args;
-use std::fs::OpenOptions;
-use std::io::prelude::*;
-use sp_core::H160;
+
 
 fn main(){
 
-	let hex_value: String = args().nth(2).unwrap()[2..].to_string();
+	let hex_value: String = args().nth(1).unwrap()[2..].to_string();
 
 	println!("{:?}", format!("{:?}", hex::decode(hex_value).unwrap()));
 
