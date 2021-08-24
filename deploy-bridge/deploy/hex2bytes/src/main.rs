78437237ad8c9cdb68f675c2441cb2d9c097a973
Gleb Urvanov <gleb.urvanov@hotovo.org>
2021-08-24 20:29:20 +0200

    Revert "added hex2bytes unnecessary first argument"

--- deploy-bridge/deploy/hex2bytes/src/main.rs
+++ deploy-bridge/deploy/hex2bytes/src/main.rs
@@ -3,7 +3,7 @@ use std::env::args;
 
 fn main(){
 
-	let hex_value: String = args().nth(2).unwrap()[2..].to_string();
+	let hex_value: String = args().nth(1).unwrap()[2..].to_string();
 
 	println!("{:?}", format!("{:?}", hex::decode(hex_value).unwrap()));
 
